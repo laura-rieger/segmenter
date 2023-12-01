0aaca310641ed8a3ce56615883702d9e9629ec6f
Laura Rieger <lauri@dtu.dk>
2023-12-01 15:11:50 +0100

    new prediction with weighting

--- src/predict.py
+++ src/predict.py
@@ -10,17 +10,31 @@ from skimage import io
 import pickle as pkl
 import numpy as np
 from unet import UNet
+
+import torch.nn.functional as F
 def get_args():
     parser = argparse.ArgumentParser(
         description="Train the UNet on images and target masks"
     )
-    parser.add_argument( "--model_name", type=str, default="9480060078", )
+    parser.add_argument( "--model_name", type=str, default="7484161710", )
     parser.add_argument("--input_folder", "-i", type=str, 
-                        default="C:\\Users\\lauri\\Documents\\GitHub\\segmenter\\data\\predict_folder\\LNO.tif") 
+                        default="C:\\Users\\lauri\\OneDrive - Danmarks Tekniske Universitet\\Dokumenter\\GitHub\\segmenter\\data\\predict_folder\\00_LNO_trainSet.tif") 
     parser.add_argument("--result_folder", 
-                        "-r", type=str, default="C:\\Users\\lauri\\Documents\\GitHub\\segmenter\\data\\result_folder") 
+                        "-r", type=str, default="C:\\Users\\lauri\\OneDrive - Danmarks Tekniske Universitet\\Dokumenter\\GitHub\\segmenter\\data\\result_folder") 
     
     return parser.parse_args()
+
+
+def get_patch_multiplier(patch_size):
+    x, y = np.meshgrid(np.arange(patch_size), np.arange(patch_size))
+
+    distance_from_middle = np.sqrt((x - patch_size / 2) ** 2 + (y - patch_size / 2) ** 2) 
+    distance_from_middle = distance_from_middle / distance_from_middle.max()
+    
+
+    distance_from_middle = 1- distance_from_middle
+    distance_from_middle = distance_from_middle +.01
+    return distance_from_middle 
 def run(results, config ):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # make model
@@ -37,33 +51,64 @@ def run(results, config ):
     #preprocess
 
     #assume that it can handle one image at a time
-    for k in tqdm(range(len(im))):
-        cur_input = (np.copy(im[k]).astype(np.float32)- results['data_min']) / (results['data_max'] - results['data_min'])
-     
-        #divide image into parts
-        # todo predict with overlap
+    
+    with torch.no_grad():
+        for k in tqdm(range(len(im))):
+            cur_input = (np.copy(im[k]).astype(np.float32)- results['data_min']) / (results['data_max'] - results['data_min'])
+        
+            #divide image into parts
 
-        height, width = cur_input.shape
-        complete_output = np.zeros((height, width))
-        for i in range(0,4):
-            for j in range(0,4):
-                cur_cur_input = cur_input[i * height//4:(i+1) * height//4, j * width//4:(j+1) * width//4]
-                cur_cur_input = torch.from_numpy(cur_cur_input).unsqueeze(0).unsqueeze(0).to(device=device)
-                cur_output = net(cur_cur_input)
-                cur_output = cur_output.squeeze(0).argmax(dim=0).cpu().detach().numpy()
-                complete_output[i * height//4:(i+1) * height//4, j * width//4:(j+1) * width//4] =cur_output
-             
+            height, width = cur_input.shape
+            step_size = 32
+            patch_size = height // 2
+            # ensure that img size is divisible by step size
+            assert height % step_size == 0 and width % step_size == 0
 
-        #save output
-        #fill up with zeros
-        complete_output_final = np.zeros_like(complete_output)
-        for val in results['class_dict']:
-            complete_output_final[complete_output == val] = results['class_dict'][val]  
-        cur_image = Image.fromarray(complete_output_final.astype(np.uint8))
-        filename = ''.join(['0' for _ in range(5-len(str(k)))]) + str(k) + ".tif"
-        full_filename = oj(results['result_folder'],filename)
-   
-        cur_image.save(full_filename)
+            # Create a meshgrid representing the coordinates of each pixel
+
+
+
+            complete_output = np.zeros(( results["num_classes"], height, width,))
+       
+            for i in range(0, height//step_size):
+                for j in range(0, width//step_size):
+                    
+                    start_height, end_height = i * step_size, i * step_size + patch_size
+                    start_width, end_width = j * step_size, j * step_size + patch_size
+
+                    cur_cur_input = cur_input[start_height:end_height, start_width:end_width]
+
+                    
+                    cur_cur_input = torch.from_numpy(cur_cur_input).unsqueeze(0).unsqueeze(0).to(device=device)
+                    cur_output = F.softmax(net(cur_cur_input), dim=1)
+                    cur_output = net(cur_cur_input)
+                    if cur_output.shape[2] != patch_size or cur_output.shape[3] != patch_size:
+                        break
+                    #flip
+                    # for flip_axes in [[-1], [-2], [-1, -2]]:
+                    #     cur_output += torch.flip(F.softmax(net(torch.flip(torch.clone(cur_cur_input), flip_axes)), dim=1), flip_axes)
+                    # # for flip
+
+                    # calculate factor with which to take this into account with a linspace
+                    factor = get_patch_multiplier(patch_size)
+                    cur_output = cur_output.squeeze().cpu().detach().numpy() * factor[None, :]
+                
+
+                    
+                    complete_output[:, start_height:end_height, start_width:end_width] +=cur_output
+
+            complete_output = np.argmax(complete_output, axis=0)
+            #save output
+            #fill up with zeros
+            complete_output_final = np.zeros_like(complete_output)
+            for val in results['class_dict']:
+                complete_output_final[complete_output == val] = results['class_dict'][val]  
+            cur_image = Image.fromarray(complete_output_final.astype(np.uint8))
+            filename = ''.join(['0' for _ in range(5-len(str(k)))]) + str(k) + ".tif"
+            full_filename = oj(results['result_folder'],filename)
+    
+            cur_image.save(full_filename)
+            
 
 
      
